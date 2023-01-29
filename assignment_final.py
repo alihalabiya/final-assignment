ab5f58842e2a8f019cf9a166ba7e6906c35506f9
alihalabiya <122269754+alihalabiya@users.noreply.github.com>
2023-01-29 15:47:23 +0200

    Add files via upload

---
+++ assignment_final.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# <b> Ali Halabiya</b><br>
+# Iam a student
+
+# Iam interested in data science because <i> its the sexiest job in the future</i>
+
+# ### The below  code count the number of characters in any word 
+
+# In[12]:
+
+
+x= input("type any word here:")
+y=str(len(x))
+print('the number of characters is : '+ ' '+ y)
+
+
+# what i learned in this week:
+# <hr>
+#     <ol>
+#     <li> how to create jupyter nootbook</li>
+#     <li><a href= "https://cloud.ibm.com/" >how to use watson studio</a></li>
+# </ol>
+#     
